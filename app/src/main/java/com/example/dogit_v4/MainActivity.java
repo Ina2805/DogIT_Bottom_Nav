32147e3a663abfd43ac55e2951db5907ddfe681e
Ina <ina.zadravec@live.com>
2022-11-12 16:01:59 +0100

    init

---
+++ app/src/main/java/com/example/dogit_v4/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.dogit_v4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavigationView navView = findViewById(R.id.fragmentContainerView3);
+
+
+    }
+}
